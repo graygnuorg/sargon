4412d11ef4e06b1c28f55b15186a38025f3edfe1
Sergey Poznyakoff <gray@gnu.org>
2024-12-03 07:36:52 +0200

    Version 2.1

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 	"sargon/server"
 )
 
-var Version = `1.91`
-var CopyleftText = `Copyright (C) 2022 Sergey Poznyakoff
+var Version = `2.1`
+var CopyleftText = `Copyright (C) 2018-2024 Sergey Poznyakoff
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
